c3cd00cfd020871a54f0a3b222efbb080a9af04c
andrew-j-brown <andrewjustinbrown@protonmail.com>
2023-12-10 14:05:44 -0600

    Created game logic. Project is complete for the time being

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Rock Paper Scissors
-The game \(° ロ°)/
+The game (° ロ°)/
+
+To play, enter your guess into the console. Spelling must be correct, but note that the guess is case agnostic. First to three points wins!
+
+### About this project
+
+To be honest, it's not the prettiest implementation. I would have loved to make the scoring system agnostic of who made which guess, and then grant the point to the parent of the winning guess. Maybe make the player and the computer both Objects, with guess and score values. Then, create a function named "score(a, b)" that could be called, assigning the point to the winning guess.
+
+Maybe I'll come back to this... but for now, I'd like to continue moving on briskly with The Odin Project course.
--- index.js
+++ index.js
@@ -1,1 +1,67 @@
 console.log("test");
+
+let valid_guess = ["rock", "paper", "scissors"];
+
+let player_score = 0;
+let computer_score = 0;
+
+function game_loop() {
+  let player_guess = prompt("Rock, Paper, or Scissors?");
+  if (valid_guess.includes(player_guess.toLowerCase())) {
+    console.log(player_guess + " was a good guess");
+
+    let computer_guess = valid_guess[Math.floor(Math.random() * valid_guess.length)]
+    console.log(computer_guess + " was guessed by the computer");
+
+    if (player_guess == computer_guess) {
+      console.log("No points this round!")
+    }
+    else if (player_guess == "rock") {
+      if (computer_guess == "scissors") {
+        player_score++;
+        console.log("You won that round!");
+        console.log("The current score is: Player - " + player_score + " | Computer - " + computer_score);
+      } else {
+        computer_score++;
+        console.log("They won that round.");
+        console.log("The current score is: Player - " + player_score + " | Computer - " + computer_score);
+      }
+    }
+    else if (player_guess == "scissors") {
+      if (computer_guess == "paper") {
+        player_score++;
+        console.log("You won that round!");
+        console.log("The current score is: Player - " + player_score + " | Computer - " + computer_score);
+      } else {
+        computer_score++;
+        console.log("They won that round.");
+        console.log("The current score is: Player - " + player_score + " | Computer - " + computer_score);
+      }
+    }
+    else if (player_guess == "paper") {
+      if (computer_guess == "rock") {
+        player_score++;
+        console.log("You won that round!");
+        console.log("The current score is: Player - " + player_score + " | Computer - " + computer_score);
+      } else {
+        computer_score++;
+        console.log("They won that round.");
+        console.log("The current score is: Player - " + player_score + " | Computer - " + computer_score);
+      }
+    }
+
+    if (player_score == 3) {
+      console.log("You win! Good job.");
+    } else if (computer_score == 3) {
+      console.log("You lost. :(");
+    } else {
+      game_loop();
+    }
+  } else {
+    console.log(player_guess + " is not a valid guess. You may choose from Rock, Paper, or Scissors.");
+    game_loop();
+  }
+
+};
+
+game_loop();
